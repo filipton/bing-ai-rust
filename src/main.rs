57fa85b8fe376cb892c8169e30b114028afe0baa
filipton <filipton12@gmail.com>
2024-02-20 12:06:39 +0100

    feat: own errors and more features

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
-use sydney::BingAIWs;
+use sydney::{BingAIWs, SydneyError};
+use tracing::{debug, error};
 
 mod json;
 mod sydney;
@@ -9,11 +10,32 @@ mod types;
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
     let mut ai = BingAIWs::new(types::Tone::Precise).await?;
-    ai.ask("My name is \"Filip\". Please respond with it when i'll ask you to do it!")
+    //ai.set_citations(true);
+    ai.set_close_ws_after(true);
+
+    ai.ask("What the weather today in Paris?").await?;
+    loop {
+        let res = ai.get_next_msgs().await;
+        match res {
+            Err(SydneyError::EndOfResponse) => {
+                debug!("End of response");
+                break;
+            }
+            Err(e) => {
+                error!("Error: {}", e);
+                break;
+            }
+            Ok(msg) => {
+                println!("Response: {:?}", msg);
+            }
+        }
+    }
+
+    /*
+    ai.ask("What is my name? (Respond with fake paris name)")
         .await?;
-    ai.parse_msgs().await?;
-    ai.ask("What is my name?").await?;
-    ai.parse_msgs().await?;
+    ai.get_next_msg().await?;
+    */
 
     tokio::signal::ctrl_c().await?;
     Ok(())
--- src/sydney.rs
+++ src/sydney.rs
@@ -2,8 +2,9 @@ use crate::types::Tone;
 use anyhow::{anyhow, Result};
 use futures_util::{future, pin_mut, StreamExt};
 use serde_json::json;
+use thiserror::Error;
 use tokio_tungstenite::{connect_async, tungstenite::Message};
-use tracing::{debug, info};
+use tracing::{debug, info, trace};
 
 const USER_AGENT: &str = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36";
 const CREATE_URL: &str = "https://www.bing.com/turing/conversation/create";
@@ -11,10 +12,44 @@ const WS_URL: &str = "wss://sydney.bing.com/sydney/ChatHub";
 const BUNDLE_VERSION: &str = "1.1586.1";
 const DELIMETER: &str = "\x1E";
 
+#[derive(Error, Debug)]
+pub enum SydneyError {
+    #[error("WebSocket not connected!")]
+    WebSocketNotConnected,
+
+    #[error("Json parsing error: {0}")]
+    JsonParsingError(#[from] serde_json::Error),
+
+    #[error("Max messages count limit reached!")]
+    MaxMessagesCountLimitReached,
+
+    #[error("Thorttling error!")]
+    ThrottlingError,
+
+    #[error("End of response")]
+    EndOfResponse,
+
+    #[error("Other error: {0}")]
+    OtherError(#[from] anyhow::Error),
+}
+
+#[derive(Debug)]
+pub enum SydneyResponse {
+    FinalText(String),
+    StreamText(String),
+    SuggestedResponses(Vec<String>),
+}
+
+#[allow(dead_code)]
 pub struct BingAIWs {
+    close_ws_after: bool,
+    citations: bool,
+    suggestions: bool,
+
     client: reqwest::Client,
 
     invocation_id: i64,
+    end_of_response: bool,
     tone: Tone,
 
     client_id: String,
@@ -28,6 +63,7 @@ pub struct BingAIWs {
     )>,
 }
 
+#[allow(dead_code)]
 impl BingAIWs {
     pub async fn new(tone: Tone) -> Result<Self> {
         let client = reqwest::ClientBuilder::new()
@@ -77,9 +113,14 @@ impl BingAIWs {
         debug!("Encrypted conversation signature: {encrypted_conversation_signature}");
 
         Ok(Self {
+            close_ws_after: false,
+            citations: false,
+            suggestions: false,
+
             client,
 
             invocation_id: 0,
+            end_of_response: true,
             tone,
 
             client_id,
@@ -90,8 +131,25 @@ impl BingAIWs {
         })
     }
 
+    /// Set whether to close ws after asking a question and receiving a response.
+    pub fn set_close_ws_after(&mut self, close: bool) {
+        self.close_ws_after = close;
+    }
+
+    /// Set whether to include citations in the response. (Like url's etc.)
+    pub fn set_citations(&mut self, citations: bool) {
+        self.citations = citations;
+    }
+
+    /// Set whether to include suggestions in the response.
+    pub fn set_suggestions(&mut self, suggestions: bool) {
+        self.suggestions = suggestions;
+    }
+
     pub async fn ask(&mut self, prompt: &str) -> Result<()> {
-        self.connect_ws().await?;
+        if self.ws.is_none() {
+            self.connect_ws().await?;
+        }
 
         let ask_json = crate::json::ask_json(
             prompt,
@@ -106,128 +164,132 @@ impl BingAIWs {
         send_ws_delim(tx, ask_json)?;
 
         self.invocation_id += 1;
+        self.end_of_response = false;
         Ok(())
     }
 
-    pub async fn parse_msgs(&mut self) -> Result<()> {
-        let rx = if let Some((_, ref mut rx)) = self.ws {
-            rx
-        } else {
-            return Err(anyhow!(""));
+    pub async fn get_next_msgs(&mut self) -> Result<Vec<SydneyResponse>, SydneyError> {
+        if self.end_of_response {
+            return Err(SydneyError::EndOfResponse);
+        }
+
+        let rx = &mut self
+            .ws
+            .as_mut()
+            .ok_or_else(|| SydneyError::WebSocketNotConnected)?
+            .1;
+
+        let mut responses = Vec::new();
+        let msg = rx.recv().await;
+        let msg_str = match msg {
+            Some(Message::Text(str)) => str,
+            _ => return Err(SydneyError::OtherError(anyhow!("Msg not text"))),
         };
 
-        loop {
-            let msg = rx.recv().await;
-            let msg_str = match msg {
-                Some(Message::Text(str)) => str,
-                _ => break,
-            };
+        for ws_str in msg_str.split(DELIMETER) {
+            if ws_str.is_empty() {
+                continue;
+            }
 
-            for ws_str in msg_str.split(DELIMETER) {
-                if ws_str.is_empty() {
+            let ws_json: serde_json::Value = serde_json::from_str(ws_str)?;
+            let typ = ws_json
+                .get("type")
+                .ok_or_else(|| anyhow!("Json type field not found!"))?;
+
+            if typ == 1 {
+                let messages = if let Some(messages) = ws_json["arguments"][0].get("messages") {
+                    messages
+                } else {
                     continue;
-                }
+                };
 
-                let ws_json: serde_json::Value = serde_json::from_str(ws_str)?;
-                let typ = ws_json
-                    .get("type")
-                    .ok_or_else(|| anyhow!("Json type field not found!"))?;
-
-                if typ == 1 {
-                    let messages = if let Some(messages) = ws_json["arguments"][0].get("messages") {
-                        messages
-                    } else {
+                // Skip "Searching in web for..." msg
+                let adaptive_cards = messages[0].get("adaptiveCards");
+                if let Some(adaptive_cards) = adaptive_cards {
+                    if adaptive_cards[0]["body"][0].get("inlines").is_some() {
                         continue;
-                    };
+                    }
 
-                    // Skip "Searching in web for..." msg
-                    let adaptive_cards = messages[0].get("adaptiveCards");
-                    if let Some(adaptive_cards) = adaptive_cards {
-                        if adaptive_cards[0]["body"][0].get("inlines").is_some() {
-                            continue;
-                        }
-
-                        // citations
-                        /*
+                    if self.citations {
                         if let Some(text) = adaptive_cards[0]["body"][0].get("text") {
-                            info!("cited text: {text}");
+                            responses.push(SydneyResponse::StreamText(text.to_string()));
                         } else {
                             let text = &adaptive_cards[0]["body"][1]["text"];
-                            info!("cited text2: {text}");
+                            responses.push(SydneyResponse::StreamText(text.to_string()));
                         }
-                        */
                     }
+                }
 
-                    // normal text
+                if !self.citations {
                     if let Some(text) = messages[0].get("text") {
-                        info!("message text: {text}");
+                        responses.push(SydneyResponse::StreamText(text.to_string()));
                     }
-                } else if typ == 2 {
-                    if let Some(throttling) = ws_json["item"].get("throttling") {
-                        let messages_count = throttling
-                            .get("numUserMessagesInConversation")
-                            .unwrap_or(&serde_json::Value::Number(serde_json::Number::from(0)))
-                            .as_i64()
-                            .unwrap_or(0);
+                }
+            } else if typ == 2 {
+                if let Some(throttling) = ws_json["item"].get("throttling") {
+                    let messages_count = throttling
+                        .get("numUserMessagesInConversation")
+                        .unwrap_or(&serde_json::Value::Number(serde_json::Number::from(0)))
+                        .as_i64()
+                        .unwrap_or(0);
 
-                        let max_messages = throttling["maxNumUserMessagesInConversation"]
-                            .as_i64()
-                            .ok_or_else(|| anyhow!("Cannot read max user msgs"))?;
+                    let max_messages = throttling["maxNumUserMessagesInConversation"]
+                        .as_i64()
+                        .ok_or_else(|| anyhow!("Cannot read max user msgs"))?;
 
-                        if messages_count == max_messages {
-                            debug!(
-                                "Max messages count limit reached! ({messages_count}/{max_messages})"
-                            );
+                    if messages_count == max_messages {
+                        debug!(
+                            "Max messages count limit reached! ({messages_count}/{max_messages})"
+                        );
 
-                            return Err(anyhow!("Error (max messages)"));
-                        }
+                        return Err(SydneyError::MaxMessagesCountLimitReached);
+                    }
+                }
+
+                let messages = if let Some(messages) = ws_json["item"].get("messages") {
+                    messages
+                } else {
+                    let result = ws_json["item"]["result"]["value"]
+                        .as_str()
+                        .unwrap_or("NOT FOUND");
+
+                    match result {
+                        "Throttled" => debug!("Throttled result (type 2 msg)"),
+                        "CaptchaChallenge" => debug!("Captcha! (type 2 msg)"),
+                        _ => {}
                     }
 
-                    let messages = if let Some(messages) = ws_json["item"].get("messages") {
-                        messages
-                    } else {
-                        let result = ws_json["item"]["result"]["value"]
-                            .as_str()
-                            .unwrap_or("NOT FOUND");
+                    return Err(SydneyError::ThrottlingError);
+                };
 
-                        match result {
-                            "Throttled" => debug!("Throttled result (type 2 msg)"),
-                            "CaptchaChallenge" => debug!("Captcha! (type 2 msg)"),
-                            _ => {}
-                        }
+                let messages = messages
+                    .as_array()
+                    .ok_or_else(|| anyhow!("Messages not found/not an array"))?;
+                let mut i = messages.len() - 1;
 
-                        return Err(anyhow!("Error (throttling)"));
-                    };
-
-                    let messages = messages
+                if let Some(adaptive_cards) = messages
+                    .last()
+                    .ok_or_else(|| anyhow!("Cannot get last msg"))?
+                    .get("adaptiveCards")
+                {
+                    let adaptive_cards = adaptive_cards
                         .as_array()
-                        .ok_or_else(|| anyhow!("Messages not found/not an array"))?;
-                    let mut i = messages.len() - 1;
-
-                    if let Some(adaptive_cards) = messages
+                        .ok_or_else(|| anyhow!("Adaptive cards not found/not an array"))?;
+                    if adaptive_cards
                         .last()
-                        .ok_or_else(|| anyhow!("Cannot get last msg"))?
-                        .get("adaptiveCards")
+                        .ok_or_else(|| anyhow!("Cannot get last adaptive card"))?["body"][0]
+                        .get("inlines")
+                        .is_some()
                     {
-                        let adaptive_cards = adaptive_cards
-                            .as_array()
-                            .ok_or_else(|| anyhow!("Adaptive cards not found/not an array"))?;
-                        if adaptive_cards
-                            .last()
-                            .ok_or_else(|| anyhow!("Cannot get last adaptive card"))?["body"][0]
-                            .get("inlines")
-                            .is_some()
-                        {
-                            i = messages.len() - 2;
-                        }
+                        i = messages.len() - 2;
                     }
+                }
 
-                    let message = messages.get(i).ok_or_else(|| {
-                        anyhow!("Message with that idx doesnt exists (impossible)")
-                    })?;
+                let message = messages
+                    .get(i)
+                    .ok_or_else(|| anyhow!("Message with that idx doesnt exists (impossible)"))?;
 
-                    // suggested responses (i will remove this or just make function to enable
-                    // them)
+                if self.suggestions {
                     if let Some(suggested_responses) = message.get("suggestedResponses") {
                         let suggested_responses: Vec<&str> = suggested_responses
                             .as_array()
@@ -236,32 +298,39 @@ impl BingAIWs {
                             .filter_map(|sr| sr["text"].as_str())
                             .collect();
 
-                        info!("suggested_responses: {suggested_responses:?}");
+                        responses.push(SydneyResponse::SuggestedResponses(
+                            suggested_responses.iter().map(|s| s.to_string()).collect(),
+                        ));
                     }
+                }
 
-                    // citations
+                if self.citations {
                     if let Some(text) = message["adaptiveCards"][0]["body"][0].get("text") {
-                        info!("final cited text: {text}");
+                        responses.push(SydneyResponse::FinalText(text.to_string()));
                     } else {
                         let text = &message["adaptiveCards"][0]["body"][1]["text"];
-                        info!("final cited text2: {text}");
+                        responses.push(SydneyResponse::FinalText(text.to_string()));
                     }
+                } else if let Some(text) = message.get("text") {
+                    responses.push(SydneyResponse::FinalText(text.to_string()));
+                }
 
-                    // normal text
-                    info!("Final text: {}", message["text"]);
-
+                if self.close_ws_after {
                     if let Some(ref mut ws) = self.ws {
                         ws.0.close_channel();
                         ws.1.close();
                     }
                     self.ws = None;
-                    //_ = clear_recv_chan(rx).await;
-                    return Ok(());
+                } else {
+                    _ = clear_recv_chan(rx).await;
                 }
+
+                self.end_of_response = true;
+                break;
             }
         }
 
-        Ok(())
+        Ok(responses)
     }
 
     async fn connect_ws(&mut self) -> Result<()> {
@@ -278,7 +347,7 @@ impl BingAIWs {
             let read_fut = {
                 read.for_each(|msg| async {
                     if let Ok(msg) = msg {
-                        debug!("WS msg: {msg:?}");
+                        trace!("WS msg: {msg:?}");
                         _ = tx_read.send(msg);
                     }
                 })
